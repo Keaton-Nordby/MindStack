3d72a861829034db1fee89e4c08afc3be8edd3ac
Keaton Nordby <keatonnordby4@gmail.com>
2026-02-03 20:46:34 -0600

    home page layout partial

--- frontend/src/Home/Home.jsx
+++ frontend/src/Home/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div>
+        <div>
+          <div>
+            <p className="text-sm font-medium">Keaton</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
